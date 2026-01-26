dad34a24b042593ce78fcac11d0877db06c69af6
kwnam0814 <kwnam0814@naver.com>
2026-01-27 00:58:49 +0900

    JS 변수와 자료형

---
+++ 4. 자바스크립트(ES6) 기본부터 심화/2. JS 변수와 자료형/variable.js
@@ -0,0 +1,27 @@
+// 변수 선언
+let box;
+box = 30;
+console.log(box); // 30
+console.log(typeof box); // number
+
+box = 20;
+console.log(box); // 20
+console.log(typeof box); // number
+
+// 문자형 자료 string
+let myName = "홍길동";
+console.log(myName); // 홍길동
+console.log(typeof myName); // string
+
+let yourName = '김철수';
+console.log(yourName); // 김철수
+console.log(typeof yourName); // string
+
+// boolean형 자료
+let isCompare = 10 > 100;
+console.log(isCompare); // false
+console.log(typeof isCompare); // boolean
+
+let isFine = 10 < 100;
+console.log(isFine); // true
+console.log(typeof isFine); // boolean
